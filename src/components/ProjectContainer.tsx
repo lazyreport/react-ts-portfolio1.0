2291090047ab3b13586b90beaa623f20ad1857a2
lazyreport <kinghyejoo@gmail.com>
2022-10-31 12:49:16 +0900

    added demo video, video.d.ts, ProjectContainer.tsx

---
+++ src/components/ProjectContainer.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import demo from "../assets/videos/demo.mp4"
+
+
+
+
+function ProjectContainer() {
+  return (
+    <div className='project-container'>
+        <div className="project-description">
+        <h2 className='project-name'>Project Name</h2>
+        <p>project summary</p>
+        <p>project feautures</p>
+        <div>project stacks</div>
+        <a href="">project links</a>
+
+        </div>
+        <div className="project-video">
+            {/* should I do Iframe of video? I think Iframe would be better.. */}
+            <video src={demo}></video>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectContainer
