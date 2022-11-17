236d6ee55343a8014177b5ecad833549fce8b53c
lazyreport <kinghyejoo@gmail.com>
2022-11-17 16:27:08 +0900

    update stacks and libs data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import "./styles/styles.scss";
-import { useTranslation, Trans } from "react-i18next";
 import LanguageSwitcher from "./layout/LanguageSwitcher";
 
-const publicProjects = ["ABC", "XYZ"];
 const App: React.FC = () => {
-  const { t, i18n } = useTranslation();
-
 
   return (
     <div className="App">
